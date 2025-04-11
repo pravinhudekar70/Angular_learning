464ce1dc447c91b1ee7bb101fb84b7cfeb36b9af
pravin hudekar <pravin.hudekar@definelabs.com>
2025-04-12 00:46:53 +0530

    button added

--- My_app/my_app/src/app/app.component.ts
+++ My_app/my_app/src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [LoginComponent,SignupComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'my_app';
+
+  handleOnClick(){
+    alert("button clicked");
+    this.fub2();
+  }
+  fub2(){
+    console.log("hihi");
+  }
 }
