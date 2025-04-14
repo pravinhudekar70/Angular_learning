eca4804ce408377046671e8e9a4b610ee2e03276
pravin hudekar <pravin.hudekar@definelabs.com>
2025-04-15 02:55:36 +0530

    user component added

--- My_app/my_app/src/app/app.component.ts
+++ My_app/my_app/src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { HeaderComponent } from "./header/header.component";
+import { UserComponent } from "./user/user.component";
 
 @Component({
   selector: 'app-root',
-  imports: [LoginComponent, SignupComponent, HeaderComponent],
+  imports: [LoginComponent, SignupComponent, HeaderComponent, UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
