5213ff76a3c80ce30d5f73ef25325acaf7832e9c
pravin hudekar <pravin.hudekar@definelabs.com>
2025-04-16 03:10:35 +0530

    user avatar added

--- My_app/my_app/src/app/user/user.component.ts
+++ My_app/my_app/src/app/user/user.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 @Component({
   selector: 'app-user',
   imports: [],
@@ -7,5 +10,22 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+selectedUser = DUMMY_USERS[randomIndex];
+
+get imagePath(){
+  return 'users/'+ this.selectedUser.avatar;
+}
+
+get userAvatar() {
+  return this.selectedUser.avatar;
+}
+get userName() {
+  return this.selectedUser.name;
+}
+ onSelectUser(){
+  console.log('Selected user:', this.selectedUser.name);
+  const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+  this.selectedUser = DUMMY_USERS[randomIndex];
+ }
 
 }
