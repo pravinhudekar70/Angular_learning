4c880f8b70c4da8437d3d29ee9d8316ec3e217d7
pravin hudekar <pravin.hudekar@definelabs.com>
2025-05-09 03:18:36 +0530

    new component added

---
+++ starting-project/src/app/dashboard/support-tickets/support-tickets.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-support-tickets',
+  standalone: true,
+  imports: [],
+  templateUrl: './support-tickets.component.html',
+  styleUrl: './support-tickets.component.css'
+})
+export class SupportTicketsComponent {
+  
+  
+}
