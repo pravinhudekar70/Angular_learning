f013b4a8774bf533a17ca905cd235d15ca202da0
pravin hudekar <pravin.hudekar@definelabs.com>
2025-05-11 01:09:30 +0530

    edit form module

--- starting-project/src/app/dashboard/server-status/server-status.component.ts
+++ starting-project/src/app/dashboard/server-status/server-status.component.ts
@@ -5,8 +5,21 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './server-status.component.html',
-  styleUrl: './server-status.component.css'
+  styleUrl: './server-status.component.css',
 })
 export class ServerStatusComponent {
-  currentStatus = 'online';
+  currentStatus: 'online' | 'offline' | 'unknown' = 'online';
+  constructor() {}
+  ngOnInit() {
+    setInterval(() => {
+      const rnd = Math.random();
+      if (rnd < 0.5) {
+        this.currentStatus = 'online';
+      } else if (rnd < 0.9) {
+        this.currentStatus = 'offline';
+      } else {
+        this.currentStatus = 'unknown';
+      }
+    }, 5000);
+  }
 }
--- starting-project/src/app/dashboard/support-tickets/new-ticket/new-ticket.component.ts
+++ starting-project/src/app/dashboard/support-tickets/new-ticket/new-ticket.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from "../../../shared/control/control.component";
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ButtonComponent, ControlComponent],
+  imports: [ButtonComponent, ControlComponent,FormsModule, CommonModule],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
-
+ onSubmit() :void {
+   console.log('Form submitted');
+ }
+ 
 }
