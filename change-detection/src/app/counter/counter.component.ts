df1f7a782a6598a362d0ea3606ea655b8b513e7c
pravin hudekar <pravin.hudekar@definelabs.com>
2025-05-18 01:59:10 +0530

    change detecting

---
+++ change-detection/src/app/counter/counter.component.ts
@@ -0,0 +1,28 @@
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+
+import { InfoMessageComponent } from '../info-message/info-message.component';
+
+@Component({
+  selector: 'app-counter',
+  standalone: true,
+  templateUrl: './counter.component.html',
+  styleUrl: './counter.component.css',
+  imports: [InfoMessageComponent],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CounterComponent {
+  count = signal(0);
+
+  get debugOutput() {
+    console.log('[Counter] "debugOutput" binding re-evaluated.');
+    return 'Counter Component Debug Output';
+  }
+
+  onDecrement() {
+    this.count.update((prevCount) => prevCount - 1);
+  }
+
+  onIncrement() {
+    this.count.update((prevCount) => prevCount + 1);
+  }
+}
