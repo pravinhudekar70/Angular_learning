ca79d58391ed9ad96f92caac084794d5ce26dfd1
pravin hudekar <pravin.hudekar@definelabs.com>
2025-05-24 03:44:53 +0530

    added validation for form

--- loginform/src/app/auth/login/login.component.ts
+++ loginform/src/app/auth/login/login.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
+  imports: [FormsModule]
+ 
 })
-export class LoginComponent {}
+export class LoginComponent {
+  onSubmit(formData:NgForm) {
+    
+    const enteredEmail = formData.value.email;
+    const enteredPassword = formData.value.password;
+    console.log(enteredEmail, enteredPassword);
+    formData.reset();
+  }
+}
