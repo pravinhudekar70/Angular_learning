a057230cad95c9d331493b38ba51aff00a2303b8
pravin hudekar <pravin.hudekar@definelabs.com>
2025-05-24 21:21:35 +0530

    login form ui done

--- loginform/src/app/auth/login/login.component.ts
+++ loginform/src/app/auth/login/login.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -9,8 +10,19 @@ import { FormsModule, NgForm } from '@angular/forms';
   imports: [FormsModule]
  
 })
-export class LoginComponent {
+export class LoginComponent implements AfterViewInit {
+  @ViewChild('form') form!: NgForm;
+
+  ngAfterViewInit() {
+    this.form.valueChanges?.pipe(debounceTime(500)).subscribe((value: { email: any; }) => {
+      window.localStorage.setItem('saved-login-form', JSON.stringify({ email: value.email }));
+    });
+  }
+
   onSubmit(formData:NgForm) {
+    if (formData.invalid) {
+      return;
+    }
     
     const enteredEmail = formData.value.email;
     const enteredPassword = formData.value.password;
