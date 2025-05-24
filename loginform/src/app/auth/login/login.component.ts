37d1dc0bbacd40c142cf7099f2853b4b74f123fc
pravin hudekar <pravin.hudekar@definelabs.com>
2025-05-25 03:15:42 +0530

    sign up page added

--- loginform/src/app/auth/login/login.component.ts
+++ loginform/src/app/auth/login/login.component.ts
@@ -1,5 +1,12 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
+import { AfterViewInit, Component, inject, OnInit, ViewChild, DestroyRef } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  NgForm,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { debounceTime } from 'rxjs';
 
 @Component({
@@ -7,26 +14,79 @@ import { debounceTime } from 'rxjs';
   standalone: true,
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
-  imports: [FormsModule]
- 
+  imports: [FormsModule, ReactiveFormsModule],
 })
-export class LoginComponent implements AfterViewInit {
-  @ViewChild('form') form!: NgForm;
+// export class LoginComponent implements AfterViewInit {
+//   @ViewChild('form') form!: NgForm;
 
-  ngAfterViewInit() {
-    this.form.valueChanges?.pipe(debounceTime(500)).subscribe((value: { email: any; }) => {
-      window.localStorage.setItem('saved-login-form', JSON.stringify({ email: value.email }));
-    });
+//   ngAfterViewInit() {
+//     this.form.valueChanges?.pipe(debounceTime(500)).subscribe((value: { email: any; }) => {
+//       window.localStorage.setItem('saved-login-form', JSON.stringify({ email: value.email }));
+//     });
+//   }
+
+//   onSubmit(formData:NgForm) {
+//     if (formData.invalid) {
+//       return;
+//     }
+
+//     const enteredEmail = formData.value.email;
+//     const enteredPassword = formData.value.password;
+//     console.log(enteredEmail, enteredPassword);
+//     formData.reset();
+//   }
+// }
+
+//using reactive forms
+export class LoginComponent  implements OnInit{
+  private destroyRef = inject(DestroyRef);
+  form = new FormGroup({
+    email: new FormControl('', {
+      validators: [Validators.email, Validators.required],
+    }),
+    password: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(6)],
+    }),
+  });
+  get emailIsValid() {
+    return (
+      this.form.controls.email.touched &&
+      this.form.controls.email.invalid &&
+      this.form.controls.email.dirty
+    );
   }
+  get passwordIsValid() {
+    return (
+      this.form.controls.password.touched &&
+      this.form.controls.password.invalid &&
+      this.form.controls.password.dirty
+    );
+  }
+  ngOnInit() {
+    const savedForm = window.localStorage.getItem('saved-login-form');
+    if (savedForm) {
+      const parsedForm = JSON.parse(savedForm);
+      this.form.patchValue({
+        email: parsedForm.email,
+      })};
 
-  onSubmit(formData:NgForm) {
-    if (formData.invalid) {
-      return;
-    }
-    
-    const enteredEmail = formData.value.email;
-    const enteredPassword = formData.value.password;
+    const subscription = this.form.valueChanges
+      .pipe(debounceTime(500))
+      .subscribe((value) => {
+        window.localStorage.setItem(
+          'saved-login-form',
+          JSON.stringify({ email: value.email })
+        );
+      });
+      this.destroyRef.onDestroy(() => {
+        subscription.unsubscribe();
+      }
+    );  
+  }
+  onSubmit() {
+    console.log(this.form);
+    const enteredEmail = this.form.value.email;
+    const enteredPassword = this.form.value.password;
     console.log(enteredEmail, enteredPassword);
-    formData.reset();
   }
 }
--- loginform/src/app/auth/signup/signup.component.ts
+++ loginform/src/app/auth/signup/signup.component.ts
@@ -1,9 +1,88 @@
 import { Component } from '@angular/core';
+import {
+  AbstractControl,
+  FormArray,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+function equalValues(control: AbstractControl) {
+  const password = control.get('password')?.value;
+  const confirmPassword = control.get('confirmPassword')?.value;
+  if (password === confirmPassword) {
+    return null;
+  }
+  return { passwordsNotEqual: true };
+}
 
 @Component({
   selector: 'app-signup',
   standalone: true,
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css',
+  imports: [FormsModule, ReactiveFormsModule],
 })
-export class SignupComponent {}
+export class SignupComponent {
+  form = new FormGroup({
+    email: new FormControl('', {
+      validators: [Validators.email, Validators.required],
+    }),
+    passwords: new FormGroup({
+      password: new FormControl('', {
+        validators: [Validators.required, Validators.minLength(6)],
+      }),
+      confirmPassword: new FormControl('', {
+        validators: [Validators.required, Validators.minLength(6)],
+      }),
+    },[equalValues]),
+
+    firstName: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(2)],
+    }),
+    lastName: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(2)],
+    }),
+    address: new FormGroup({
+      street: new FormControl('', {
+        validators: [Validators.required, Validators.minLength(2)],
+      }),
+      number: new FormControl('', {
+        validators: [Validators.required, Validators.minLength(10)],
+      }),
+      postalCode: new FormControl('', {
+        validators: [Validators.required, Validators.minLength(2)],
+      }),
+      city: new FormControl('', {
+        validators: [Validators.required, Validators.minLength(2)],
+      }),
+    }),
+
+    role: new FormControl<
+      'student' | 'teacher' | 'employee' | 'founder' | 'other'
+    >('student', {
+      validators: [Validators.required],
+    }),
+    source: new FormArray(
+      [new FormControl(false), new FormControl(false), new FormControl(false)],
+      {
+        validators: [Validators.required],
+      }
+    ),
+    agree: new FormControl(false, {
+      validators: [Validators.requiredTrue],
+    }),
+  });
+
+  onSubmit() {
+    if (this.form.invalid) {
+      console.log('Form is invalid');
+      return;
+    }
+    console.log(this.form.value);
+  }
+  onReset() {
+    this.form.reset();
+  }
+}
